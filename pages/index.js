8e781e07535f4bbe0a00844f40dc0f4a2406494f
David Steel <webdevsteel@gmail.com>
2021-04-06 09:15:03 +1200

    fetching and showing data

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
 // Head used for custom titles, meta tags, keywords etc
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
         <title>WebDev News</title>
         <meta name='keywords' content='web development, programming' />
       </Head>
-      <h1>Welcome to Next</h1>
+      {articles.map((article) => (
+        <>
+          <h3>{article.title}</h3>
+          <p>{article.body}</p>
+        </>
+      ))}
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles,
+    },
+  }
 }
