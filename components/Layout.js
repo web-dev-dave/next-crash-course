ca89d3da61531aaf39e95debe95f6562f96ebe5e
David Steel <webdevsteel@gmail.com>
2021-04-06 09:02:53 +1200

    custom document

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,18 @@
+import Nav from '../components/Nav'
+import Header from '../components/Header'
 import styles from '../styles/Layout.module.css'
 
 const Layout = ({ children }) => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>{children}</main>
-    </div>
+    <>
+      <Nav />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Header />
+          {children}
+        </main>
+      </div>
+    </>
   )
 }
 
